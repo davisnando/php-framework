d966521b484fb553b57402157379114d7be58b87
Nando Davis <nandodavis@gmail.com>
2017-04-19 19:15:27 +0200

    Menu fix

--- admin/templates/main/menu.php
+++ admin/templates/main/menu.php
@@ -20,7 +20,7 @@
         <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
           <ul class="nav nav-pills flex-column">
             <li class="nav-item">
-              <a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="/admin/dashboard">Overview </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/admin/role">Role</a>
