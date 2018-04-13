aa6e0e3c2969cc2a540abb4a3759a839f39bedc2
Nando Davis <nandodavis@gmail.com>
2018-04-13 12:28:50 +0200

    Fix templateapp view

--- templateapp/view.php
+++ templateapp/view.php
@@ -4,7 +4,10 @@ function index(){
     echo "Hello, ";
     // $personal = Personal::Create(['firstname'=>'test', 'lastname'=>'test123']);
     // $user = Users::Create(['username'=>'test','password'=>sha1('test'),'email'=>"email@gmail.com"  , 'Personal'=>$personal->id]);
-    $u = userRole::get(['id'=>$_GET['id']]);
+    $u = null;
+    if(array_key_exists('id', $_GET)){
+        $u = userRole::get(['id'=>$_GET['id']]);
+    }
     if($u != null)
         // SELECT Personal.firstname, Personal.lastname FROM userRole JOIN Users on userRole.user=Users.id JOIN Personal on Users.Personal=Personal.id WHERE userRole=:id
         echo $u->user->object->personal->object->firstname->value." ".$u->user->object->personal->object->lastname->value;
